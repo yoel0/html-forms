c76a44c40f355617d3968d628b8d7d72e68dd7c4
yoel <yoel@macbook-pro.home>
2020-07-23 11:35:43 -0700

    [html] add boilerplate code

---
+++ app.js
@@ -0,0 +1,12 @@
+/*let endPoint = `https://api.yelp.com/search?q=${}apiKey=${apiKey}`;
+
+fetch (endPoint)
+.then( response => {
+  return response.json();
+})
+.then(date => {
+  console.log(data);
+})
+.catch(error => {
+  console.log(error);
+});*/
